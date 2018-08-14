241b8cff47860c4ae5f1026c04cb5734051f037a
Alfen Febral <alfenfebral@gmail.com>
2018-08-14 20:21:57 +0700

    remove factory comment

--- factory-function.js
+++ factory-function.js
@@ -6,11 +6,3 @@ const dog = () => {
 };
 const sniffles = dog();
 sniffles.talk();
-
-/* 
-  when not to use factory ?
-  when you need create a lot of object
-  ------------------------------------
-  Factory Benchmark: 0.0004 ms
-  Class Benchmark: 0.0002 ms
-*/
